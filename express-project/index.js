20b78f0b54c5c7f8c12e8f50a2635b55d96ec48f
SehrShamir <sehrshamir786@gmail.com>
2026-03-13 17:29:11 +0530

    Middleware in Express

--- express-project/index.js
+++ express-project/index.js
@@ -3,7 +3,24 @@ let express = require("express")
 let app = express()
 
 app.use(express.json())
+let myToken = "12345"
 
+let checkToken = (req, res, next) => {
+    if (req.query.token == "" || req.query.token == undefined) {
+        return res.send({
+            status: 0,
+            msg: "please fill the token"
+        })
+    }
+    if (req.query.token != myToken) {
+        return res.send({
+            status: 0,
+            msg: "please fill the  correct token"
+        })
+    }
+    next();
+}
+app.use(checkToken)  // Middleware
 app.get("/", (req, res) => {
     res.send({ status: 1, msg: "HOme Page API" })
 })
@@ -21,6 +38,7 @@ app.get('/producst', (req, res) => {
 })
 app.post("/login", (req, res) => {
     console.log(req.body)
+    // res.status(200).json()
     res.send({ status: 1, msg: "new API" })
 })
 
