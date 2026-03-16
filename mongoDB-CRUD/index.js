eb927a9c511641d770f346a435dda3001edb0045
SehrShamir <sehrshamir786@gmail.com>
2026-03-16 14:40:01 +0530

    Delete and update

--- mongoDB-CRUD/index.js
+++ mongoDB-CRUD/index.js
@@ -1,5 +1,6 @@
 let express = require("express");
 const { dbConnection } = require("./dbConnection");
+const { ObjectId } = require("mongodb");
 let app = express();
 
 app.use(express.json())
@@ -19,16 +20,59 @@ app.post("/student-insert", async (req, res) => {
         sName,
         sEmail
     };
-    // let { sName, sEmail } = req.body;
-    // let obj = (sName, sEmail);
-    console.log(obj);
-    let insertRes = await studentCollection.insertOne(obj);
-    let resObj = {
-        status: 1,
-        msg: "Data Insert",
-        insertRes
-    };
-    res.send(resObj);
+    let checkEmail = await studentCollection.findOne({ sEmail })
+    if (checkEmail == null) {
+        // let { sName, sEmail } = req.body;
+        // let obj = (sName, sEmail);
+        console.log(obj);
+        let insertRes = await studentCollection.insertOne(obj);
+        let resObj = {
+            status: 1,
+            msg: "Data Insert",
+            insertRes
+        };
+        res.send(resObj);
+    }
 })
 
+app.delete("/student-delete/:id", async (req, res) => {
+    let { id } = req.params;
+    let myDB = await dbConnection();
+    let studentCollection = myDB.collection("students")
+    let delRes = await studentCollection.deleteOne({ _id: new ObjectId(id) })
+
+    let resOjb = {
+        status: 1,
+        msg: "Data Delete",
+        delRes
+    }
+    res.send(resOjb)
+})
+
+app.put("/student-update/:id", async (req, res) => {
+    let { id } = req.params;
+    let { sName, sEmail } = req.body;
+
+    let obj = {}
+    if (sName != "" && sName !== undefined && sName !== null) {
+        obj['sName'] = sName
+    }
+    if (sEmail != "" && sEmail !== undefined && sEmail !== null) {
+        obj['sEmail'] = sEmail
+    }
+    let myDB = await dbConnection();
+
+    let studentCollection = myDB.collection("students")
+    let updateRes = await studentCollection.updateOne({ _id: new ObjectId(id) }, {
+        $set:
+            obj
+
+    })
+    let resObj = {
+        status: 1,
+        msg: "Data Update",
+        updateRes
+    }
+    res.send(resObj)
+})
 app.listen("8000")
